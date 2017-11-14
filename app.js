61ad4fd5d6dc835f6f2a117a2cf25cb1e78610d9
Lawrence Kay <lkay9495@hotmail.com>
2017-11-14 23:25:34 +0900

    Applied HTTPS Certificates * config.js : Not updated. Spearately updated for security reason

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 var builder = require('botbuilder');
 var restify = require('restify');
+var fs = require('fs');
+var config = require('./config');
 
-
-var server = restify.createServer();
+var serverOption = {
+	url: 'b4d.lkaybob.pe.kr',
+	certificates: fs.readFileSync(config.certPath + 'fullchanin.pem'),
+	key: fs.readFileSync(config.certPath + 'privkey.pem')
+}
+var server = restify.createServer(serverOption);
 server.listen(process.env.port || process.env.PORT || 3978, function () {
 	console.log("%s listening to %s", server.name, server.url);
 });
 
 var connector = new builder.ChatConnector({
-	appId: process.env.MICROSOFT_APP_ID,
-	appPassword: process.env.MICROSOFT_APP_PASSWORD
+	appId: process.env.MICROSOFT_APP_ID || config.appId,
+	appPassword: process.env.MICROSOFT_APP_PASSWORD || config.appPassword
 });
 
 server.post('/api/messages', connector.listen());
-
-var connector = new builder.ConsoleConnector().listen();
 
 var bot = new builder.UniversalBot(connector, function (session) {
     session.send("You said: %s", session.message.text);
